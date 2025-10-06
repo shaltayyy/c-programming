bf4490dba91109fcaa255ae3ac2cbac3ff465def
shaltayyy <lukicevzahar@gmail.com>
2025-10-06 17:25:18 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double array[10];
+    double sum = 0.0;
+    
+   
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+        sum += array[i];
+    }
+    
+   
+    double average = sum / 10.0;
+    printf("%.2f\n", average);
+    
     return 0;
 }
