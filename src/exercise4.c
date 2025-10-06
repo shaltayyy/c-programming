ce4170b7d62ea423b9ce3e8578c64d795a722ea1
shaltayyy <lukicevzahar@gmail.com>
2025-10-06 17:23:45 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double array[10];
+    
+    
+    if (argc != 3) {
+        printf("Usage: %s <X> <K>\n", argv[0]);
+        return 1;
+    }
+    
+    double X = atof(argv[1]);
+    int K = atoi(argv[2]);
+    
+    
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+    }
+    
+   
+    for(int i = 9; i > K; i--) {
+        array[i] = array[i-1];
+    }
+    
+    
+    array[K] = X;
+    
+    
+    for(int i = 0; i < 10; i++) {
+        printf("%g", array[i]);
+        if (i < 9) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
     return 0;
 }
