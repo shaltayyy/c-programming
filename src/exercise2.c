4a85d90df2873e85d5689b5c0f0e7704d3d35662
shaltayyy <lukicevzahar@gmail.com>
2025-10-06 17:24:43 +0700

    Update exercise2.c

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double array[10];
+    
+    
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+    }
+    
+    
+    for(int i = 9; i >= 0; i--) {
+        printf("%g", array[i]);
+        if (i > 0) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
     return 0;
 }
